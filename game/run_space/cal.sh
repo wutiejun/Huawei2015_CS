e872f1bfed6ffd32ed0b640aea0d52af3a379431
wu.tiejun <wu.country@gmail.com>
2015-06-10 14:25:15 +0800

    完善脚本，支持汇总。

--- game/run_space/cal.sh
+++ game/run_space/cal.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-echo "Cal log info:"
-echo "==========================================="
+printf "%-18s: %5s %5s\r\n" "Type" "Total" "Win";
+echo "===========================================";
+all_total=0;
+all_win=0;
 for type in ROYAL_FLUSH STRAIGHT_FLUSH FOUR_OF_A_KIND FULL_HOUSE FLUSH STRAIGHT THREE_OF_A_KIND TWO_PAIR ONE_PAIR HIGH_CARD; do 
-	printf "%-18s:" "$type"
-	grep -w "$type" ./log.txt | wc -l;
+	total=$(grep -w "$type" ./log.txt | wc -l);
+	win=$(grep -w "$type" ./log.txt | grep "1:"| wc -l);
+	printf "%-18s: %5d %5d\r\n" "$type" $total $win;
+	all_total=`expr $all_total + $total`;
+	all_win=`expr $all_win + $win`;
 done
+echo "===========================================";
+printf "%-18s: %5d %5d\r\n" "All Total" $all_total $all_win;
 
 echo "Cal msg info:"
 echo -n "Get rounds:"
