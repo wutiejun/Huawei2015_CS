9962334707c52fbcfae8699a90914104a18dfec2
wu.tiejun <wu.country@126.com>
2015-06-14 10:33:24 +0800

    添加脚本。
    Signed-off-by: wu.tiejun <wu.country@126.com>

---
+++ game/run_space/3.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+. ./evn.sh
+
+# user 1
+export "PLAYER1_IP"=127.0.0.1
+export "PLAYER1_PORT"=6001
+export "PLAYER1_ID"=1001
+
+# user 2
+export "PLAYER2_IP"=127.0.0.2
+export "PLAYER2_PORT"=6002
+export "PLAYER2_ID"=1002
+
+# user 3
+export "PLAYER3_IP"=127.0.0.3
+export "PLAYER3_PORT"=6003
+export "PLAYER3_ID"=1003
+
+# Start Server
+./gameserver -gip $ser_ip -seq replay -r $reg -d $delay -m $money -b $blind -t $jetton -h $hand 0</dev/null 1>/dev/null 2>/dev/null &
+
+# Start all users
+./game 127.0.0.1 6000 127.0.0.1 6001 1001  $1 &
+
+#./game 127.0.0.1 6000 127.0.0.2 6002 1002 &
+./check 127.0.0.1 6000 127.0.0.2 6002 1002 &
+
+./check 127.0.0.1 6000 127.0.0.3 6003 1003 &
---
+++ game/run_space/stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+killall game
+killall check
+killall gameserver
+
